90c33e4ce4bbf06f8ff8ac530817e1c74b3942d9
wildrodeo <sophie.bismuth@gmail.com>
2021-09-20 22:39:00 +0200

    Add project files.

---
+++ MyDatabaseContext/dbo/Tables/Todoes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Todoes] (
+    [ID]          INT            IDENTITY (1, 1) NOT NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    [CreatedDate] DATETIME       NOT NULL,
+    CONSTRAINT [PK_dbo.Todoes] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
